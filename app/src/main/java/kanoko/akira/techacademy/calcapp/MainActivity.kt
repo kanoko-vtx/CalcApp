83520aa1049ca3fb2cb6502418c0a822e820b50a
kanoko-vtx <a,kanoko@vortex-gi.com>
2019-04-09 17:23:38 +0900

    アプリが落ちる

--- app/src/main/java/kanoko/akira/techacademy/calcapp/MainActivity.kt
+++ app/src/main/java/kanoko/akira/techacademy/calcapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import android.support.design.widget.Snackbar
 import android.util.Log
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -22,11 +23,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
-
         val anserView: TextView = findViewById(R.id.anser)
+        val num1edittext1 = findViewById<EditText>(R.id.textbox1)
+        val num1 = num1edittext1.text.toString().toInt()
+        val num1edittext2 = findViewById<EditText>(R.id.textbox2)
+        val num2 = num1edittext2.text.toString().toInt()
+
+        // num2が0だったら警告
 
         // textbox1.2が空だったら警告
-        if (textbox1.text.isNotBlank() and textbox2.text.isNotBlank()) {
+
+        if (textbox1.text.isNotBlank() or textbox2.text.isNotBlank()) {
 
         }else{
             val Snackbar = Snackbar.make(rootlayout, "数字を入力してください", Snackbar.LENGTH_LONG)
@@ -35,7 +42,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         when(v.id){
             R.id.plus -> {
-                anserView.text = "足す"
+                val r = num1 + num2
+                Log.d("calclog","$num1, $num2, $r")
+                anserView.text = getString(r)
             }
             R.id.minus -> {
                 anserView.text = "引く"
